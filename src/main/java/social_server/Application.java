72b3be16d30909d7c5adb060baa5000dea6afec8
Vlasenko <vlasenko-pav@yandex.ru>
2016-08-22 15:10:37 +0700

    commit

--- src/main/java/social_server/Application.java
+++ src/main/java/social_server/Application.java
@@ -2,11 +2,15 @@ package social_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by SBT-Vlasenko-PV on 22.08.2016.
  */
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
